b7930d4ba2e3914feff56cc0dc782e73903065df
Bradley-Hill <97234446+Bradley-Hill@users.noreply.github.com>
2023-04-09 17:08:19 +0200

    added javascript validation

--- script.js
+++ script.js
@@ -1,13 +1,59 @@
 const form = document.getElementById("form");
 const button = document.getElementById("button");
 
-form.addEventListener("submit",function(event) {
-    const firstName = document.getElementById("first-name").value;
-    const email = document.getElementById("email").value;
-    const password = document.getElementById("password").value;
-    const confirm-password = document.getElementById("confirm-password").value;
+form.addEventListener("submit", function (event) {
+  const firstName = document.getElementById("first-name").value;
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+  const confirmPassword = document.getElementById("confirm-password").value;
+  const emailRegex = new RegExp("([a-zA-Z0-9\\_\\-\\.]+)@([a-zA-Z]+).(.+)");
+  const passwordRegex = new RegExp("^[a-zA-Z0-9]*$");
+  const phoneRegex = new RegExp("^[0-9]{12}$");
 
+  if (
+    firstName === "" ||
+    email === "" ||
+    password === "" ||
+    confirm - password === ""
+  ) {
+    alert("Please fill all required fields");
+    event.preventDefault();
+    return false;
+  }
 
-button.addEventListener("click", () => {
-  form.submit();
+  if (
+    password.length < 8 ||
+    password.length > 20 ||
+    passwordRegex.test(password)
+  ) {
+    alert(
+      "Password must be between 8 and 20 characters, no special characters."
+    );
+    event.preventDefault();
+    return false;
+  }
+
+  if (password !== confirmPassword) {
+    alert("Passwords do not match");
+    event.preventDefault();
+    return false;
+  }
+
+  if (phone !== "" && !phoneRegex.test(phone)) {
+    alert("Invalid phone number");
+    event.preventDefault();
+    return false;
+  }
+
+  if (!emailRegex.test(email)) {
+    alert("Invalid email address");
+    event.preventDefault();
+    return false;
+  }
+
+  return true;
+
+  button.addEventListener("click", () => {
+    form.submit();
+  });
 });
