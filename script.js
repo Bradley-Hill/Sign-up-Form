674a2e5937ebcbf8adbadda3739ae900102e7dd1
Bradley-Hill <97234446+Bradley-Hill@users.noreply.github.com>
2023-04-09 16:55:19 +0200

    adding JS validation

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 const form = document.getElementById("form");
 const button = document.getElementById("button");
 
+form.addEventListener("submit",function(event) {
+    const firstName = document.getElementById("first-name").value;
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+    const confirm-password = document.getElementById("confirm-password").value;
+
+
 button.addEventListener("click", () => {
   form.submit();
 });
